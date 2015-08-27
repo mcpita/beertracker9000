647a15c452e5a807348c07cedd51a17e0195c970
mcpita <anita.mcallister@gmail.com>
2015-08-27 16:06:47 -0400

    added text field and button

---
+++ src/BeerTracker9000/js/app.js
@@ -0,0 +1,14 @@
+﻿angular.module("beerTracker", []);
+var app = angular.module("beerTracker");
+app.run(function (){
+    
+});
+
+
+app.controller("AddBeerCtrl", ["$scope", function ($scope) {
+    $scope.add = function () {
+        $scope.name = "";
+        $scope.saved = true;
+    };
+
+}]);
